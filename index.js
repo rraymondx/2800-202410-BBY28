1147f3b44eb192888b3416d6d01717d18ddf42c4
ray <rraymondxie@gmail.com>
2024-05-07 12:38:00 -0700

    Created utils.js; Added basic NavBar, Cards, and Footer.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
+require("./utils.js");
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
+const path = require('path');
 // const saltRounds = 15;
 
 const port = process.env.PORT || 3000;
@@ -12,7 +14,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
