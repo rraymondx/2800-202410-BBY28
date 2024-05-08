23872858bd10e0d73cdae6d0c314f837e276a362
Pouyan Norouzi <irfn7pouyan@gmail.com>
2024-05-08 10:16:48 -0700

    Created an alternative file setup

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/html', express.static(__dirname + '/public/html'));
+app.use('/img', express.static(__dirname + '/public/img'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+
+app.get('/tsunami', (req, res) => {
+    res.sendFile(__dirname + '/public/html/tsunami.html');
 });
 
 app.listen(port, () => {
