265067866dc15d6d654db04d898ce508aca48b97
lucaslav05 <lucaslaviolette05@gmail.com>
2024-05-14 13:33:39 -0700

    Added Earthquake.ejs and put html into it, also added res.render to '/earthquake'.

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.get('/wildfire', (req, res) => {
 
 //earthquake info page
 app.get('/earthquake', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/html/earthquake.html'));
+    res.render("earthquake");
 });
 
 //flood info page
