9c74672af98f1aa17542b0d05562e356614933af
lucaslav05 <lucaslaviolette05@gmail.com>
2024-05-22 17:06:03 -0700

    Added easter egg to tornado page on image click.

---
+++ scripts/tornado.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var image = document.getElementById("myImage");
+    var audio = document.getElementById("myAudio");
+
+    if (image && audio) {
+        image.addEventListener("click", function() {
+
+            audio.play();
+
+            // Remove the class if it's already present to restart the animation
+            image.classList.remove('tornado-effect');
+
+            // Use setTimeout to ensure the class removal is processed
+            setTimeout(() => {
+                image.classList.add('tornado-effect');
+            }, 50);
+        });
+    } else {
+        console.error("Image or audio element not found");
+    }
+});
+
