64b87d5f3aaa72843e9129b1b197e5a1ca95951e
lucaslav05 <lucaslaviolette05@gmail.com>
2024-05-23 14:48:39 -0700

    Fixed incorrect link and fixed styling on volunteer page.

--- index.js
+++ index.js
@@ -496,9 +496,9 @@ app.get('/volunteer', (req, res) => {
             link: 'https://bcsara.com/volunteer/'
         },
         {
-            image: '/img/ems.jpg',
-            info: 'Volunteer with EMS.',
-            link: 'https://vancouver.ca/home-property-development/emergency-social-services.aspx'
+            image: '/img/StJohns.jpg',
+            info: 'Volunteer with St. John Ambulance.',
+            link: 'https://sja.ca/en/community-services'
         },
         {
             image: '/img/disrel.jpg',
