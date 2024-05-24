7d2d85a2aca6e60f288698a1279b6755e972efdc
Batmanpouknight <irfn7pouyan@gmail.com>
2024-05-23 20:17:17 -0700

    Added animation to the wildfire easter egg, Fixed bug where profile pic would disappear, Improved css for when footer is present

--- scripts/wildfire.js
+++ scripts/wildfire.js
@@ -1,17 +1,17 @@
 var clicks = 0;
-document.addEventListener("DOMContentLoaded", function() {
-    var image = document.getElementById("myHero");
-    var audio = document.getElementById("myAudio");
 
-    if (image && audio) {
-        image.addEventListener("click", function() {
-            clicks++;
-            if (clicks == 3) {
-                audio.play();
-                clicks = 0;
-            }
-        });
-    } else {
-        console.error("Image, audio, or flood effect element not found");
-    }
+$(function () {
+    $('#myHero').click(function () {
+        const fireElement = $("<div class='fire'></div>").text('');
+        clicks++;
+        if (clicks == 3) {
+            $('#myAudio')[0].play();
+            clicks = 0;
+            $('.content')[0].after(fireElement[0]);
+            setTimeout(function(){
+                $('.fire').remove();
+                $('#myAudio')[0].pause();
+            }, 3000);
+        }
+    });
 });
