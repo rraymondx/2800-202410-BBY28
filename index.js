67da949cf3d6a5131d40ac5a98f8f331b39d7b40
ray <rraymondxie@gmail.com>
2024-05-29 10:07:06 -0700

    Changed indents and spacing and added comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//required modules
+// Required modules
 require(__dirname + "/modules/utils.js");
 require(__dirname + "/modules/authentication.js");
 require(__dirname + "/modules/database.js");
@@ -15,22 +15,23 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const bodyParser = require('body-parser');
 
-//number used for encrypting passwords
+
+// Number used for encrypting passwords
 const saltRounds = 15;
 
-//port address
+
+// Port address
 const port = process.env.PORT || 3000;
 const app = express();
 
 
-
-//allows parsing of url encoded body
+// Allows parsing of url encoded body
 app.use(express.urlencoded({ extended: false }));
-//parse json bodies
+// Parse json bodies
 app.use(bodyParser.json());
-
-//setup for ejs
+// Setup for ejs
 app.set('view engine', 'ejs');
+
 
 const cloudinary = require('cloudinary');
 const axios = require('axios');
@@ -41,7 +42,7 @@ cloudinary.config({
 });
 
 
-//session creation
+// Session creation
 app.use(session({
     secret: process.env.NODE_SESSION_SECRET,
     store: mongoStore,
@@ -51,12 +52,12 @@ app.use(session({
 ));
 
 
-//nodemailer setup
-//transporter is going to be an object that is able to send mail
+// Nodemailer setup
+// Transporter is going to be an object that is able to send mail
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
-    secure: false, // Use `true` for port 465, `false` for all other ports
+    secure: false, 
     auth: {
         type: "OAuth2",
         user: "disasternotresetpass@gmail.com",
@@ -64,12 +65,11 @@ const transporter = nodemailer.createTransport({
         clientSecret: process.env.OAUTH_CLIENT_SECRET,
         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
         accessUrl: "https://accounts.google.com/o/oauth2/token"
-        // accessToken: process.env.OAUTH_ACCESS_TOKEN
     },
 });
 
 
-//Check if conncetion works
+// Check if conncetion works
 transporter.verify(function (error, success) {
     if (error) {
         console.log(error);
@@ -78,7 +78,8 @@ transporter.verify(function (error, success) {
     }
 });
 
-//returns true if a user exists based on a filter
+
+// Returns true if a user exists based on a filter
 async function userExists(filterName, filterValue) {
     let filter = {};
     filter[filterName] = filterValue;
@@ -86,7 +87,8 @@ async function userExists(filterName, filterValue) {
     return !(searchUser == null);
 }
 
-//Code to check if the session is validated
+
+// Code to check if the session is validated
 function isValidSession(req) {
     if (req.session.authenticated) {
         return true;
@@ -95,6 +97,7 @@ function isValidSession(req) {
 }
 
 
+// Session Validation
 function sessionValidation(req, res, next) {
     if (isValidSession(req)) {
         next();
@@ -104,7 +107,8 @@ function sessionValidation(req, res, next) {
     }
 }
 
-// static paths for routing files
+
+// Static paths for routing files
 app.use('/js', express.static(__dirname + '/scripts'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/html', express.static(__dirname + '/public/html'));
@@ -112,7 +116,7 @@ app.use('/img', express.static(__dirname + '/public/img'));
 app.use('/audio', express.static(__dirname + '/public/audio'));
 
 
-// root
+// Root
 app.use('/', (req, res, next) => {
     var currUrl = url.parse(req.url).pathname;
     app.locals.picId = req.session.picId;
@@ -130,23 +134,24 @@ app.use('/', (req, res, next) => {
         } else {
             app.locals.disasterName = undefined;
         }
-        // app.locals.cssFile =  cssFiles[req.url];
     }
     next();
 });
 
 
-//home page
+// Home page
 app.get('/', sessionValidation, (req, res) => {
-        res.render("home");
+    res.render("home");
 });
 
-//landing page
+
+// Landing page
 app.get('/landing', (req, res) => {
     res.render('landing');
 });
 
-//signup page
+
+// Signup page
 app.get('/signup', (req, res) => {
     //If an error happens an error code is sent through query
     var errorCode = req.query.error;
@@ -158,11 +163,11 @@ app.get('/signup', (req, res) => {
 });
 
 
-//submit signup and store user in database
+// Submit signup and store user in database
 app.post('/signupSubmit', signupSubmit);
 
 
-//login page
+// Login page
 app.get('/login', (req, res) => {
     var errorCode = req.query.error;
     var errorStrings = ["Email format invalid", "Email does not exist <a href='/signup'>Sign Up?</a>", "Password is incorrect"];
@@ -170,14 +175,16 @@ app.get('/login', (req, res) => {
 });
 
 
-//signup submit, searches for matching emails and pass
+// Signup submit, searches for matching emails and pass
 app.post('/loginSubmit', loginSubmit);
+
 
 // Profile page
 app.get('/profile', sessionValidation, async (req, res) => {
     const user = await userCollection.findOne({ email: req.session.email });
     res.render('profile', { user: user });
 });
+
 
 // Profile update
 app.post('/profileSubmit', sessionValidation, upload.single('picture'), async (req, res) => {
@@ -190,13 +197,13 @@ app.post('/profileSubmit', sessionValidation, upload.single('picture'), async (r
     let update = {};
     if (changed == 'true') {
         let buf64 = req.file.buffer.toString('base64');
-        stream = await cloudinary.uploader.upload("data:image/png;base64," + buf64, function (result) { 
+        stream = await cloudinary.uploader.upload("data:image/png;base64," + buf64, function (result) {
             console.log(result);
-            update = { $set: { username: username, email: email, city: city, picId: result.url} };
+            update = { $set: { username: username, email: email, city: city, picId: result.url } };
             req.session.picId = result.url;
             app.locals.picId = result.url;
         }, { folder: 'profilePics' });
-        
+
     } else {
         update = { $set: { username: username, email: email, city: city } };
     }
@@ -212,7 +219,8 @@ app.post('/profileSubmit', sessionValidation, upload.single('picture'), async (r
     res.redirect('/profile');
 });
 
-//smartAI chat page
+
+// smartAI chat page
 app.get('/smartAI', sessionValidation, (req, res) => {
     // used for sending server variable to client side
     app.locals.open_ai_key = process.env.OPEN_AI_KEY;
@@ -220,13 +228,14 @@ app.get('/smartAI', sessionValidation, (req, res) => {
 });
 
 
-//A page that holds the list of disasters
+// A page that holds the list of disasters
 app.use('/disasterList', sessionValidation);
 app.get('/disasterList', (req, res) => {
     res.render("disasterList");
 });
 
-//A page for displaying specific disaster information
+
+// A page for displaying specific disaster information
 app.use('/disasterList/:disasterName', sessionValidation);
 app.get('/disasterList/:disasterName', (req, res) => {
     const disasterName = req.params.disasterName;
@@ -234,16 +243,17 @@ app.get('/disasterList/:disasterName', (req, res) => {
 });
 
 
-//A catch-all checklist for disasters
+// A catch-all checklist for disasters
 app.use('/checklist', sessionValidation);
 app.get('/checklist', async (req, res) => {
     const savedChecklist = req.session.checklist;
-    res.render("checklist", {savedChecklist: savedChecklist});
+    res.render("checklist", { savedChecklist: savedChecklist });
 });
 
-//Save checklist data
+
+// Save checklist data
 app.post('/checklistSave', sessionValidation, async (req, res) => {
-    const checklistData  = req.body;
+    const checklistData = req.body;
     let array = [];
     for (let i = 0; i < 10; i++) {
         if ('item' + i in checklistData) {
@@ -253,12 +263,12 @@ app.post('/checklistSave', sessionValidation, async (req, res) => {
         }
     }
     req.session.checklist = array;
-    await userCollection.updateOne({email: req.session.email}, {$set: {checklist: array}});
+    await userCollection.updateOne({ email: req.session.email }, { $set: { checklist: array } });
     res.redirect('/checklist');
 });
 
 
-//forgot password page
+// Forgot password page
 app.get('/forgot-password', (req, res) => {
     var errorCode = req.query.error;
     var errorString = ["Email format invalid", "The Email does not exist"]
@@ -266,7 +276,7 @@ app.get('/forgot-password', (req, res) => {
 });
 
 
-//forgot password submit
+// Forgot password submit
 app.post('/forgotPasswordSubmit', async (req, res) => {
     var email = req.body.email;
 
@@ -291,8 +301,6 @@ app.post('/forgotPasswordSubmit', async (req, res) => {
         from: "disasternotresetpass@gmail.com",
         to: email,
         subject: "Reset your password",
-        // text: "pls work pls",
-        //need to change this link when deploying
         html: `<p>follow this link to reset your password: <a href='https://two800-202410-bby28.onrender.com/reset-password?id=${resetPassId}'>Link</a></p>`
     }
 
@@ -302,7 +310,7 @@ app.post('/forgotPasswordSubmit', async (req, res) => {
 });
 
 
-//reset password page
+// Reset password page
 app.get('/reset-password', async (req, res) => {
     var id = req.query.id;
     if (!id) {
@@ -323,15 +331,15 @@ app.get('/reset-password', async (req, res) => {
 });
 
 
-//reset password submit
+// Reset password submit
 app.post('/resetPasswordSubmit', async (req, res) => {
     var { username, newPassword, confirmPassword } = req.body;
-    //Checks that the password is confirmed
+    // Checks that the password is confirmed
     if (newPassword !== confirmPassword) {
         res.send('passwords arent equal');
         return;
     }
-    //validation using joi
+    // Validation using joi
     const schema = Joi.string().max(20).required();
     const validationResult = schema.validate(newPassword);
     if (validationResult.error != null) {
@@ -340,10 +348,12 @@ app.post('/resetPasswordSubmit', async (req, res) => {
     }
     var hashedPassword = await bcrypt.hash(newPassword, saltRounds);
     await userCollection.updateOne({ username: username }, { $set: { password: hashedPassword } });
-    // redirect to login page
+    // Redirect to login page
     res.redirect('/login');
 });
 
+
+// Volunteer Page
 app.get('/volunteer', sessionValidation, (req, res) => {
     const volunteers = [
         {
@@ -365,17 +375,17 @@ app.get('/volunteer', sessionValidation, (req, res) => {
             link: 'https://www.samaritanspurse.ca/getinvolved/canadian-disaster-relief/'
         }
     ];
-
     res.render('volunteer', { volunteers });
 });
 
 
+// Contacts Page
 app.get('/contacts', (req, res) => {
     res.render('contacts');
 });
 
 
-
+// Predict Damage
 app.post('/predictDamage', async (req, res) => {
     const { disaster } = req.body;
     try {
@@ -415,20 +425,19 @@ app.post('/predictDamage', async (req, res) => {
 });
 
 
-//logout page, destroys session and returns to home page
+// Logout page, destroys session and returns to home page
 app.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 });
 
-//404 page
+// 404 page
 app.get('*', (req, res) => {
     res.status(404);
     res.render("404");
-    //res.send("Page Not Found - 404");
 });
 
-//localhost set up
+// Localhost set up
 app.listen(port, () => {
     console.log(`Node.js Application listening at ${port}`);
 });
